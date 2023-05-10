b0d8f6e84396a54735a69cb8cb76bd644eb44f5c
Daalderink <112620415+Daalderink@users.noreply.github.com>
2023-05-10 08:52:47 +0200

    victory sound toegevoegd, verder gewerkt aan projectpagina en de about me pagina geupdatet

--- js/projects.js
+++ js/projects.js
@@ -10,6 +10,11 @@ const projects = [
     },{
         title: "Slender: The Missing Soul",
         description: "Ik ben voor het keuzedeel gamedesign bezig om een videogame te maken. Ik werk hier dan samen in een groep.",
+        imgURL: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2014_23/491626/140606-slender-man-mn-735.jpg"
+    },{
+        title: "The Norse man",
+        description: "Ik ben van plan om een videogame te maken die te maken heeft met de noorse mythologie. Het gaat gedeelte een beetje gebaseerd zijn op de nieuwere God of War games.",
+        imgURL: "https://historyuk.s3.eu-west-2.amazonaws.com/s3fs-public/2022-02/norse%20mythology-min.jpg"
     }
 ]
 
