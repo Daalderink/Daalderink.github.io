4f5032807d31f4df1e08a432aef162b3124ee58c
Daalderink <112620415+Daalderink@users.noreply.github.com>
2023-09-26 20:18:30 +0200

    movies pagina gemaakt

--- daveflix/movies.js
+++ daveflix/movies.js
@@ -7,8 +7,8 @@ fetch(`/daveflix/movies.json`)
 function showMovies(data){
     for (let index = 0; index < data.movies.length; index++) {
         const movie = data.movies[index];
-        if(index < 4){
-            dump.innerHTML += `<div class="col-md-3"><div class="card">
+        
+            dump.innerHTML += `<div class="col-md-3 pb-4"><div class="card">
             <img class="card-img-top" src="${movie.bannerimg}" alt="Title">
             <div class="card-body">
                 <h4 class="card-title">${movie.title}</h4>
@@ -18,7 +18,7 @@ function showMovies(data){
                 </p>
             </div>
         </div></div>`
-        }
+        
         
     }
 }
