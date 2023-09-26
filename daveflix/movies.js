391887e1df11f158ec0ecae62da82ba1644dfb65
Daalderink <112620415+Daalderink@users.noreply.github.com>
2023-09-26 20:37:39 +0200

    film toegevoegd

--- daveflix/movies.js
+++ daveflix/movies.js
@@ -7,8 +7,7 @@ fetch(`/daveflix/movies.json`)
 function showMovies(data){
     for (let index = 0; index < data.movies.length; index++) {
         const movie = data.movies[index];
-        
-            dump.innerHTML += `<div class="col-md-3 pb-4"><div class="card">
+        dump.innerHTML += `<div class="col-md-3 pb-4"><div class="card">
             <img class="card-img-top" src="${movie.bannerimg}" alt="Title">
             <div class="card-body">
                 <h4 class="card-title">${movie.title}</h4>
@@ -18,7 +17,5 @@ function showMovies(data){
                 </p>
             </div>
         </div></div>`
-        
-        
     }
 }
