35d69c9cb25015f2b02b002801471914f26cc02e
daalderink <112620415+Daalderink@users.noreply.github.com>
2024-03-26 21:40:06 +0100

    test

---
+++ project.php
@@ -0,0 +1,14 @@
+<?php
+$project = 'Boter Kaas en Bitches';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=  ?></title>
+</head>
+<body>
+    
+</body>
+</html>
